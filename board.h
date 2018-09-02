18c1c08540e87775d1b3ccbf80d38971c330fa10
dla1996 <denwisla@yahoo.com>
2018-09-02 12:24:21 -0400

    Adding board class to unclutter main

---
+++ board.h
@@ -0,0 +1,24 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+
+#define M 20
+#define N 10
+class GameBoard
+{
+public:
+
+	// Constructor
+	GameBoard(void);
+
+private:
+	unsigned int width;
+	unsigned int height;
+
+};
+
+
+
+
+#endif __ BOARD_H__
+#pragma once
